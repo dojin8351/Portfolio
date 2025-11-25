fb01eb4e4a48f28d4aec34ac802fc579e29f6ef3
김도현 <dojin8351@gmail.com>
2025-11-25 17:47:43 +0900

    style : CompetencyCard 스타일 수정

--- src/components/CompetencyCard.tsx
+++ src/components/CompetencyCard.tsx
@@ -9,23 +9,23 @@ interface CompetencyCardProps {
 
 export default function CompetencyCard({ logos, title, description }: CompetencyCardProps) {
   return (
-    <div className="flex flex-col sm:flex-row gap-5">
-      <div className="p-6 sm:p-8 rounded-lg w-full sm:w-[160px] sm:max-w-[160px] h-auto sm:h-[160px] sm:max-h-[160px] min-w-[160px] min-h-[160px] flex-shrink-0 bg-gray-200 dark:bg-gray-800 grid grid-cols-2 gap-2 place-items-center mx-auto sm:mx-0">
-          {logos.map((img, index) => (
-            <div key={index} className="w-12 h-12 relative flex-shrink-0">
-              <Image
-                src={img.src}
-                alt={img.alt}
-                fill
-                className="object-contain"
-                sizes="64px"
-              />
-            </div>
-          ))}
-      </div>
+    <div className="flex flex-col gap-2">
       <div className="flex-1">
         <p className="text-xl sm:text-h2 font-semibold dark:text-white">{title}</p>
         <p className="text-sm sm:text-base text-gray-600 dark:text-gray-400 mt-2">{description}</p>
+      </div>
+      <div className={'flex flex-row gap-1'}>
+        {logos.map((img, index) => (
+          <div key={index} className="w-5 h-5 relative flex-shrink-0">
+            <Image
+              src={img.src}
+              alt={img.alt}
+              fill
+              className="object-contain"
+              sizes="64px"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
