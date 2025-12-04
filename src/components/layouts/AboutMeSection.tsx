5fa954288eaabbe01c19b2d282b71d9efe34b97b
김도현 <dojin8351@gmail.com>
2025-12-04 19:41:59 +0900

    fix : 브라우저 호환성 최신화

--- src/components/layouts/AboutMeSection.tsx
+++ src/components/layouts/AboutMeSection.tsx
@@ -15,52 +15,52 @@ interface AboutMeProps {
 export default function AboutMeSection({ aboutMeInfo }: AboutMeProps) {
   return (
     <SectionLayout sectionId="about-me">
+      <div
+        className={
+          "flex w-full flex-col items-center gap-3 sm:flex-row sm:items-center sm:gap-4 md:gap-6 lg:gap-10"
+        }
+      >
+        <ProfileAvatar src={aboutMeInfo?.profileImg} />
         <div
           className={
-            "flex w-full flex-col items-center gap-3 sm:flex-row sm:items-center sm:gap-4 md:gap-6 lg:gap-10"
+            "flex w-full flex-col gap-2 font-semibold sm:w-auto dark:text-white"
           }
         >
-          <ProfileAvatar src={aboutMeInfo?.profileImg} />
-          <div
+          <h1
             className={
-              "flex w-full flex-col gap-2 font-semibold sm:w-auto dark:text-white"
+              "wrap-break-words md:text-h1 text-lg leading-tight sm:text-xl md:text-2xl"
             }
           >
-            <h1
-              className={
-                "text-lg leading-tight break-words sm:text-xl md:text-2xl md:text-h1"
-              }
-            >
-              <span>안녕하세요,</span>{" "}
-              <span className={"inline-block sm:inline"}>
-                <span className={"text-green-400"}>
-                  &#34;나중보다 지금 더 일하는&#34;
-                </span>{" "}
-                <span>{aboutMeInfo?.job}</span> <span>개발자</span>{" "}
-                <span className={"text-blue-400"}>{aboutMeInfo?.name}</span>
-                <span>입니다.</span>
-              </span>
-            </h1>
-            <div className={"flex flex-row flex-wrap gap-1"}>
-              <EmailClipboardButton email={aboutMeInfo?.email} />
-              <GitHubLinkButton githubLink={aboutMeInfo?.githubLink} />
-            </div>
+            <span>안녕하세요,</span>{" "}
+            <span className={"inline-block sm:inline"}>
+              <span className={"text-green-400"}>
+                &#34;나중보다 지금 더 일하는&#34;
+              </span>{" "}
+              <span>{aboutMeInfo?.job}</span> <span>개발자</span>{" "}
+              <span className={"text-blue-400"}>{aboutMeInfo?.name}</span>
+              <span>입니다.</span>
+            </span>
+          </h1>
+          <div className={"flex flex-row flex-wrap gap-1"}>
+            <EmailClipboardButton email={aboutMeInfo?.email} />
+            <GitHubLinkButton githubLink={aboutMeInfo?.githubLink} />
           </div>
         </div>
-        <ul
-          className={
-            "mt-4 w-full space-y-2 text-xs text-gray-700 sm:mt-6 sm:text-sm md:mt-8 md:text-base dark:text-gray-400"
-          }
-        >
-          {aboutMeInfo?.introductions?.map((intro, index) => (
-            <li key={index} className={"break-words"}>
-              • {intro}
-            </li>
-          ))}
-        </ul>
-        {aboutMeInfo?.achievements && (
-          <AchievementTimeline achievements={aboutMeInfo.achievements} />
-        )}
-      </SectionLayout>
+      </div>
+      <ul
+        className={
+          "mt-4 w-full space-y-2 text-xs text-gray-700 sm:mt-6 sm:text-sm md:mt-8 md:text-base dark:text-gray-400"
+        }
+      >
+        {aboutMeInfo?.introductions?.map((intro, index) => (
+          <li key={index} className={"break-words"}>
+            • {intro}
+          </li>
+        ))}
+      </ul>
+      {aboutMeInfo?.achievements && (
+        <AchievementTimeline achievements={aboutMeInfo.achievements} />
+      )}
+    </SectionLayout>
   )
 }
